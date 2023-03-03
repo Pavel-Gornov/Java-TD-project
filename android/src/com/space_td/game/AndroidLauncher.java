445f55114864c85e0aabc4a76f866c4ce3d8fa11
Pavel-Gornov <gornovPavel30@yandex.ru>
2023-03-03 23:44:33 +0400

    0.1

---
+++ android/src/com/space_td/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.space_td.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.space_td.game.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
