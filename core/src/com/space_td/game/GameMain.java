a14123f1d270cb9b8768b86e00fc816bda2ca0bc
Pavel-Gornov <gornovpavel30@yandex.ru>
2023-03-13 19:12:32 +0400

    0.2

--- core/src/com/space_td/game/GameMain.java
+++ core/src/com/space_td/game/GameMain.java
@@ -1,34 +1,42 @@
 package com.space_td.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
+
 public class GameMain extends ApplicationAdapter {
-	private TiledMap tmap;
-	private OrthogonalTiledMapRenderer renderer;
-	private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private Texture planet;
+    //Screen = Scr
+    public float ScrWidth;
+    public float ScrHeight;
 
-	@Override
-	public void create () {
-		tmap = new TiledMap();
-		renderer = new OrthogonalTiledMapRenderer(tmap, 1 / 16f);
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 30, 20);
-		camera.update();
-	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(0.7f, 0.7f, 1.0f, 1);
-		camera.update();
-		renderer.setView(camera);
-		renderer.render();
-	}
-	
-	@Override
-	public void dispose () {
-	}
+    @Override
+    public void create() {
+        planet = new Texture("planet.png");
+        batch = new SpriteBatch();
+    }
+    @Override
+    public void resize(int width, int height){
+        ScrWidth=width;
+        ScrHeight=height;
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0, 0, 0, 1);
+        batch.begin();
+        batch.draw(planet, ScrWidth / 2, ScrHeight / 2);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        planet.dispose();
+    }
 }
