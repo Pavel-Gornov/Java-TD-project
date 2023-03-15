0327d77b25be447d814f8660c8a92b9bf333b723
Pavel-Gornov <gornovpavel30@yandex.ru>
2023-03-15 18:57:50 +0400

    code1

--- core/src/com/space_td/game/GameMain.java
+++ core/src/com/space_td/game/GameMain.java
@@ -8,74 +8,52 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-import java.awt.Window;
-
 
 public class GameMain extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture planet;
     private Texture TEMP;
-
-    //Screen = Scr
     public float ScrWidth;
     public float ScrHeight;
-    //Мы размещаем всё в пределах 400х300 экрана и умножаем на ssm{x или y}.
-    public float dfWinSizeX=640;
-    public float dfWinSizeY=480;
-    //модификатор размера окна.
-    public float ssmX;
-    public float ssmY;
+    public float planet_x;
+    public float planet_y;
     OrthographicCamera camera = new OrthographicCamera();
 
 
     @Override
     public void create() {
-
-        camera.setToOrtho(false, ScrWidth, ScrHeight);
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         planet = new Texture("planet.png");
-        //замени потом на плэйсхолдер. только прошу, не из пустоти иди (VoidiGo)
         TEMP = new Texture("planet.png");
         batch = new SpriteBatch();
         ScrHeight=Gdx.graphics.getHeight();
         ScrWidth=Gdx.graphics.getWidth();
-        calcSSM();
+        batch.setProjectionMatrix(camera.combined);
+        planet_x = Gdx.graphics.getWidth()/2f;
+        planet_y = Gdx.graphics.getHeight()/2f;
+        camera.update();
     }
 
 
     @Override
     public void render() {
-
-        camera.update();
-
-        //batch.setProjectionMatrix(camera.combined);
         ScreenUtils.clear(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-//        batch.draw(planet, 200*ssmX, 150*ssmY);
-//        batch.draw(TEMP, 16*ssmX, 128*ssmY);
-        batch.draw(planet, Gdx.graphics.getWidth()/2f, Gdx.graphics.getHeight()/2f);
-        System.out.println("W: "+Gdx.graphics.getWidth()+" H: "+Gdx.graphics.getHeight());
+        batch.draw(planet, planet_x, planet_y);
         batch.end();
+        System.out.println("W: "+Gdx.graphics.getWidth()+" H: "+Gdx.graphics.getHeight());
+        camera.update();
     }
-    @Override
-    public void resize(int width, int height){
-//        ScrWidth=width;
-//        ScrHeight=height;
-        ScrHeight=Gdx.graphics.getHeight();
-        ScrWidth=Gdx.graphics.getWidth();
-        calcSSM();
-    }
+//    @Override
+//    public void resize(int width, int height){
+//        ScrHeight=Gdx.graphics.getHeight();
+//        ScrWidth=Gdx.graphics.getWidth();
+//    }
 
     @Override
     public void dispose() {
         batch.dispose();
         planet.dispose();
-        TEMP.dispose();
     }
-    public void calcSSM(){
-        ssmX=ScrWidth/dfWinSizeX;
-        ssmY=ScrHeight/dfWinSizeY;
-
-    }
-
 }
