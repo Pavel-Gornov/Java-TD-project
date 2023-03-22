4971caaea44042072f7c7020d87a4c108f3ffc34
Minemaster656 <glebbasmanov123@gmail.com>
2023-03-22 18:17:05 +0400

    А я не знаю, мы строим лиф на луну из костылей. часть 6 Утилиты

---
+++ core/src/com/space_td/game/Utils_.java
@@ -0,0 +1,10 @@
+package com.space_td.game;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Utils_ {
+
+    public Vector2 calcCenter(float sizeX, float sizeY){
+        return new Vector2(sizeX/2f, sizeY/2f);
+    }
+}
