c85daf3e7a47d650bebdf865572ed947bca37670
Minemaster656 <glebbasmanov123@gmail.com>
2023-06-16 14:17:32 +0400

    На сегодня хватит

--- core/src/com/space_td/game/data.java
+++ core/src/com/space_td/game/data.java
@@ -1,6 +1,8 @@
 package com.space_td.game;
 
 public class data {
+    //TODO: не удалять data
+    //TODO: не удалять этот TODO и выше
     public static float gameDifficulty;
     public static float gameSpeed=1f;
     public static boolean partyMode=false;
