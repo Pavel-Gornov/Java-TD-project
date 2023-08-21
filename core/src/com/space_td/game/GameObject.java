19793515389f64ed36b00ef8544810bc2d61c044
Minemaster656 <glebbasmanov123@gmail.com>
2023-08-21 11:19:56 +0400

    Ввод GameObject и Utils, ЧИИИИИСТКА моего (говно)кода, абстракции - НЕТ!

---
+++ core/src/com/space_td/game/GameObject.java
@@ -0,0 +1,152 @@
+package com.space_td.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Octree;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Disposable;
+
+
+public class GameObject<ColliderType extends Octree.Collider> implements Disposable {
+    public Vector2 position;
+    public float rotation;
+    public Vector2 size;
+    public Vector2 scale;
+    public Vector2 originPoint;
+    public TextureRegion texture;
+    public Animation<Texture> animation;
+    boolean flipX;
+    boolean flipY;
+    boolean showColliders = false;
+
+    public ColliderType collider;
+    public String id;
+    public float velocity;
+    Vector2 oldPos;
+    public boolean isDestroyed;
+    public Texture textureFromRegion;
+
+
+    public GameObject(Vector2 position, float rotation, Vector2 size, Vector2 scale, Vector2 originPoint, TextureRegion texture, boolean flipX, boolean flipY) {
+        this.position = position;
+        this.rotation = rotation;
+        this.size = size;
+        this.scale = scale;
+        this.originPoint = originPoint;
+        this.texture = texture;
+        this.flipX = flipX;
+        this.flipY = flipY;
+        this.id = this.getClass().getName() + Utils.randInt(0, 10000) + "";
+        this.isDestroyed = false;
+        this.textureFromRegion = Utils.getTextureFromTextureRegion(this.texture);
+    }
+
+    public GameObject(Vector2 position, float rotation, Vector2 size, Vector2 scale, TextureRegion texture, boolean flipX, boolean flipY) {
+        this.position = position;
+        this.rotation = rotation;
+        this.size = size;
+        this.scale = scale;
+        this.originPoint = new Vector2(size.x / 2, size.y / 2);
+        this.texture = texture;
+        this.flipX = flipX;
+        this.flipY = flipY;
+        this.id = this.getClass().getName() + Utils.randInt(0, 10000) + "";
+        this.isDestroyed = false;
+        this.textureFromRegion = Utils.getTextureFromTextureRegion(this.texture);
+    }
+
+    public GameObject(Vector2 position, float rotation, Vector2 scale, TextureRegion texture, boolean flipX, boolean flipY) {
+        this.position = position;
+        this.rotation = rotation;
+        this.size = new Vector2(texture.getRegionWidth(), texture.getRegionHeight());
+        this.scale = scale;
+        this.originPoint = new Vector2(size.x / 2, size.y / 2);
+        this.texture = texture;
+        this.flipX = flipX;
+        this.flipY = flipY;
+        this.id = this.getClass().getName() + Utils.randInt(0, 10000) + "";
+        this.isDestroyed = false;
+        this.textureFromRegion = Utils.getTextureFromTextureRegion(this.texture);
+    }
+
+    public GameObject(Vector2 position, float rotation, TextureRegion texture, Vector2 scale, Vector2 originPoint, boolean flipX, boolean flipY) {
+        this.position = position;
+        this.rotation = rotation;
+        this.size = new Vector2(texture.getRegionWidth(), texture.getRegionHeight());
+        this.scale = scale;
+        this.originPoint = originPoint;
+        this.texture = texture;
+        this.flipX = flipX;
+        this.flipY = flipY;
+        this.id = this.getClass().getName() + Utils.randInt(0, 10000) + "";
+        this.isDestroyed = false;
+        this.textureFromRegion = Utils.getTextureFromTextureRegion(this.texture);
+    }
+
+
+
+    public void draw(Batch batch) {
+        //TODO:демона в виде нижних двух строчек оставить но не выпускать. Это нам ещё пригодится.
+//        this.originPoint.x=this.position.x*this.size.x*this.scale.x/2;
+//        this.originPoint.y=this.position.y*this.size.y*this.scale.y/2;
+
+        if (animation != null) {
+            batch.draw(animation.getKeyFrame(0f), position.x, position.y, size.x, size.y);
+        } else {
+            if (!batch.isDrawing())
+                batch.begin();
+
+            if (this.flipX & this.scale.x > 0)
+                this.scale.x *= -1;
+
+            if (this.flipY & this.scale.y > 0)
+                this.scale.y *= -1;
+
+            batch.draw(texture, this.position.x, this.position.y, this.originPoint.x, this.originPoint.y, this.size.x, this.size.y, this.scale.x, this.scale.y, this.rotation);
+            batch.setShader(null);
+
+            if (this.showColliders) {
+                onShowColliders();
+            }
+        }
+        if (oldPos != null) {
+            velocity = this.position.dst(oldPos);
+        }
+        oldPos = position;
+    }
+
+    public void onShowColliders(){
+
+    }
+
+    public void onDestroy() {
+        this.isDestroyed = true;
+    }
+
+    public void destroy() {
+        onDestroy();
+        this.dispose();
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    public void moveForward(float dist) {
+
+        float angleRad = (float) Math.toRadians(this.rotation);
+
+
+        float x = this.position.x + dist * (float) Math.cos(angleRad);
+        float y = this.position.y + dist * (float) Math.sin(angleRad);
+
+        this.position.set(x, y);
+    }
+}
---
+++ core/src/com/space_td/game/Utils.java
@@ -0,0 +1,183 @@
+package com.space_td.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.TextureData;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Utils {
+    private static final Random random = new Random();
+
+    public static Vector2 calcCenter(float sizeX, float sizeY) {
+        return new Vector2(sizeX / 2f, sizeY / 2f);
+    }
+
+    public static Texture getTextureFromRegion(TextureRegion region) {
+        TextureData data = region.getTexture().getTextureData();
+        data.prepare();
+        Texture texture = new Texture(data);
+        data.disposePixmap();
+
+        return texture;
+    }
+
+    public static ArrayList<Texture> splitTexture(Texture texture, int sizeX, int sizeY) {
+        ArrayList<Texture> textures = new ArrayList<Texture>();
+        int rows = texture.getHeight() / sizeY;
+        int cols = texture.getWidth() / sizeX;
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                textures.add(Utils.getTextureFromRegion(new TextureRegion(texture, i * sizeX, j * sizeY, sizeX, sizeY)));
+            }
+        }
+        return textures;
+    }
+
+    public static ArrayList<TextureRegion> splitRegion(Texture texture, int sizeX, int sizeY) {
+        ArrayList<TextureRegion> textures = new ArrayList<TextureRegion>();
+        int rows = texture.getHeight() / sizeY;
+        int cols = texture.getWidth() / sizeX;
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                textures.add(new TextureRegion(texture, i * sizeX, j * sizeY, sizeX, sizeY));
+            }
+        }
+        return textures;
+    }
+
+    public static float randFloat(float a, float b) {
+        if (a > b) {
+            float temp = a;
+            a = b;
+            b = temp;
+        }
+        if (a == b)
+            return a;
+        float diff = b - a;
+        return (float) (Math.random() * diff) + a;
+
+    }
+
+    public static int randInt(int a, int b) {
+        int diff = b - a;
+        return (int) (Math.floor(Math.random() * (diff + 1)) + a);
+    }
+
+    public static boolean randBoolean() {
+        return Math.random() >= 0.500001;
+    }
+
+    public static float getAngle(Vector2 point, Vector2 position) {
+        // Находим вектор от объекта до точки point
+        Vector2 targetDirection = point.cpy().sub(position);
+
+        // Находим угол между вектором от объекта до точки и осью x
+        float angle = (float) Math.atan2(targetDirection.y, targetDirection.x);
+
+        // Приводим угол к градусам
+        angle = (float) Math.toDegrees(angle);
+
+        return angle;
+    }
+
+    public static Color getGradientColor(Vector2 topLeftPos, Color topLeftColor, Vector2 bottomRightPos, Color bottomRightColor, Vector2 point) {
+        // Получаем ширину и высоту экрана
+        int screenWidth = Gdx.graphics.getWidth();
+        int screenHeight = Gdx.graphics.getHeight();
+
+        // Рассчитываем коэффициенты для перевода координат из пикселей в диапазон [0, 1]
+        float xFactor = 1f / screenWidth;
+        float yFactor = 1f / screenHeight;
+
+        // Координаты цветовых точек в диапазоне [0, 1]
+        float x1 = topLeftPos.x * xFactor;
+        float y1 = (screenHeight - topLeftPos.y) * yFactor;
+        float x2 = bottomRightPos.x * xFactor;
+        float y2 = (screenHeight - bottomRightPos.y) * yFactor;
+
+        // Создаем два цвета-вершины градиента
+        Color color1 = new Color(topLeftColor);
+        Color color2 = new Color(bottomRightColor);
+
+        // Рассчитываем длину вектора градиента
+        float gradientLength = new Vector2(x2 - x1, y2 - y1).len();
+
+        // Рассчитываем расстояние от точки до начала и конца градиента
+        float distanceFromStart = new Vector2(point.x * xFactor - x1, (screenHeight - point.y) * yFactor - y1).len();
+        float distanceFromEnd = new Vector2(point.x * xFactor - x2, (screenHeight - point.y) * yFactor - y2).len();
+
+        // Рассчитываем позицию точки на градиенте
+        float positionOnGradient = 1f - (distanceFromStart / gradientLength);
+
+        // Создаем новый цвет с учетом позиции на градиенте
+        Color color = new Color();
+        color.set(color1);
+        color.lerp(color2, positionOnGradient);
+
+        return color;
+    }
+
+    public static void doNothing() {
+        String nothing = "";
+    }
+
+        public static Vector2 mousePosToWorldPos(){
+//        Vector3 mousePos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+//        camera.unproject(mousePos);
+//        Vector2 worldMousePos = new Vector2(mousePos.x, mousePos.y);
+//        return worldMousePos;
+            return null; //TODO: пока не трогать.
+    }
+    public static float median(float a, float b) {
+        return (a + b) / 2;
+    }
+
+    public static Color randColor() {
+        return new Color((float) Math.random(), (float) Math.random(), (float) Math.random(), (float) Math.random());
+    }
+
+    public static float getDTime(float gameSpeed) {
+        return Gdx.graphics.getDeltaTime() * gameSpeed; //data.gameSpeed; //TODO: сделать скорость игры переменной
+    }
+
+    public static float getDTime() {
+        return Gdx.graphics.getDeltaTime() * 1; //data.gameSpeed; //TODO: сделать скорость игры переменной
+    }
+
+
+    public static float roundFloat(float flt, int scale, RoundingMode mode) {
+        BigDecimal bd = BigDecimal.valueOf(flt);//Великое Зло возвращается!
+        bd = bd.setScale(scale, mode);
+        return bd.floatValue();
+    }
+
+    public static Texture getTextureFromTextureRegion(TextureRegion textureRegion) {
+
+
+        Texture texture = textureRegion.getTexture();
+
+        int regionX = textureRegion.getRegionX();
+        int regionY = textureRegion.getRegionY();
+        int regionWidth = textureRegion.getRegionWidth();
+        int regionHeight = textureRegion.getRegionHeight();
+
+        TextureRegion fragment = new TextureRegion(texture, regionX, regionY, regionWidth, regionHeight);
+        return fragment.getTexture();
+    }
+
+    public static int mirrorY(int y, int space) {
+        return space - y - 1;
+    }
+
+
+}
